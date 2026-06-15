ef6452db90fea0a72fffaed025c68e58bda66220
Queenode <omodunni241@gmail.com>
2026-06-15 10:26:09 +0100

    feat(formatters): add formatBytes duplicate fallback

---
+++ lib/formatters/formatBytes.ts
@@ -0,0 +1,1 @@
+export const formatBytes = (bytes: number, decimals = 2) => { if (!+bytes) return '0 Bytes'; const k = 1024, dm = decimals < 0 ? 0 : decimals, sizes = ['Bytes', 'KiB', 'MiB', 'GiB'], i = Math.floor(Math.log(bytes) / Math.log(k)); return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`; };
