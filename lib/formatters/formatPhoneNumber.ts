c48265f6b35092d85867f4b289d209d1c21545f7
Queenode <omodunni241@gmail.com>
2026-06-15 10:26:09 +0100

    feat(formatters): add formatPhoneNumber utility

---
+++ lib/formatters/formatPhoneNumber.ts
@@ -0,0 +1,1 @@
+export const formatPhoneNumber = (str: string) => { const cleaned = ('' + str).replace(/\D/g, ''); const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/); return match ? '(' + match[1] + ') ' + match[2] + '-' + match[3] : null; };
