a9bccae654f929182b91251fe753eceddcab0d99
Queenode <omodunni241@gmail.com>
2026-06-22 19:54:04 +0100

    feat(icons): add CloudIcon component

---
+++ components/icons/CloudIcon.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const CloudIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className={'icon icon-cloudicon ' + (props.className || '')}
+    {...props}
+  >
+    {/* Example SVG Path for CloudIcon */}
+    <circle cx='12' cy='12' r='10' />
+    <path d='M12 8v8M8 12h8' />
+  </svg>
+);
