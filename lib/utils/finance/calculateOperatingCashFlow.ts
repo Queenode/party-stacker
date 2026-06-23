7cfddd6a6d88c38c85aecafc7f2e80ce608512e8
Queenode <omodunni241@gmail.com>
2026-06-23 21:24:24 +0100

    feat(finance): add calculateOperatingCashFlow utility

---
+++ lib/utils/finance/calculateOperatingCashFlow.ts
@@ -0,0 +1,1 @@
+export const calculateOperatingCashFlow = (netIncome: number, nonCashExpenses: number, changesInWorkingCapital: number): number => netIncome + nonCashExpenses + changesInWorkingCapital;
